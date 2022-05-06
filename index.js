99e894d93f4b2156b7e3a0f8d4c08aedcdf54b44
afolabiopakunle <opakunleafolabi@gmail.com>
2022-05-07 00:12:35 +0100

    power implemented

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const square = function(x) {
 }
 
 console.log(square(32));
+
+
+const power = function(base, exponent) {
+  let result = 1;
+  for(let i = 0; i < exponent; i++) {
+    result = result * base;
+  }
+
+  return result
+}
+
+console.log(power(3, 4));
