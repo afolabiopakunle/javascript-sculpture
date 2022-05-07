58d5531a5ab4f5de852d44f38071edb529cc88ff
afolabiopakunle <opakunleafolabi@gmail.com>
2022-05-07 08:32:03 +0100

    ingredients measure

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ const power = function(base, exponent) {
 }
 
 console.log(power(3, 4));
+
+const moinmoin = function(productionCount) {
+  if(!productionCount) {
+    return console.log('enter valid ammount')
+  }
+
+  const ingredients = function(sizeRequired, unit, size) {
+    let requiredAmount = sizeRequired * productionCount;
+    if(requiredAmount > 1) {
+      unit += 's'
+    }
+
+    console.log(`${requiredAmount} ${unit} of ${size}`)
+  }
+
+  ingredients(5, 'litre', 'water');
+  ingredients(0.4, 'gram', 'salt')
+}
+
+moinmoin(1)
